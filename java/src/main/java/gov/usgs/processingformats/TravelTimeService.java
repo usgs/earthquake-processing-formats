4f1b1c33d76fb36210333cdcf1d2945962a4b689
John Patton <jpatton@usgs.gov>
2020-12-03 10:23:50 -0700

    second quick tweak to add TravelTimeService

---
+++ java/src/main/java/gov/usgs/processingformats/TravelTimeService.java
@@ -0,0 +1,14 @@
+package gov.usgs.processingformats;
+
+/** TravelTimeService provides an interface for processing a TravelTimeRequest. */
+public interface TravelTimeService {
+
+  /**
+   * Process a TravelTimeRequest
+   *
+   * @param request traveltime inputs.
+   * @return calculated traveltime.
+   * @throws LocationException if errors occur.
+   */
+  public TravelTimeException getTravelTimes(final TravelTimeRequest request) throws TravelTimeException;
+}
